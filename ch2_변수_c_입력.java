764ff305eb4a553d976edadf3cc69577adb84c6d
wingunkh <99gusrms@naver.com>
2022-07-13 22:11:59 +0900

    Scanner클래스

--- ch2_변수_c_입력.java
+++ ch2_변수_c_입력.java
@@ -1,7 +1,7 @@
 //자바는 Scanner 클래스를 이용하여 화면으로부터 입력 받는다.
 import java.util.*; //Scanner 클래스는 java.util 패키지에 내장되어 있다.
 
-public class ch2_c_입력 {
+public class ch2_변수_c_입력 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in); //Scanner 클래스의 객체 생성
 		
